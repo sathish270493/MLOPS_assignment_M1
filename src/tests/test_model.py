2f14e5790725dbbc2a0bda9bea8366084d2feba5
R Sathish <sathish.r@alstomgroup.com>
2025-01-24 19:39:10 +0530

    space error correction

--- src/tests/test_model.py
+++ src/tests/test_model.py
@@ -1,6 +1,7 @@
 import unittest
 from sklearn.linear_model import LinearRegression
 
+
 class TestModel(unittest.TestCase):
     def test_model_instance(self):
         model = LinearRegression()
@@ -13,5 +14,6 @@ class TestModel(unittest.TestCase):
         model.fit(X, y)
         self.assertGreater(len(model.coef_), 0)
 
-if _name_ == "_main_":
+
+if __name__ == "_main_":
     unittest.main()
